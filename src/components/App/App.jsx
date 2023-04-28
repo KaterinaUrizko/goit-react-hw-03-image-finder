835a3952ca13a079e85369547489279ed6c982d8
Katerina Urizko <93221162+KaterinaUrizko@users.noreply.github.com>
2023-04-28 14:08:17 +0300

    Homework

---
+++ src/components/App/App.jsx
@@ -0,0 +1,110 @@
+import React, { Component } from 'react';
+import { fetchImages } from '../API/Api';
+import { Searchbar } from '../Searchbar/Searchbar';
+import { Loader } from '../Loader/Loader';
+import { ImageGallery } from '../ImageGallery/ImageGallery';
+import { Button } from '../Button/Button';
+import { animateScroll } from 'react-scroll';
+import { Modal } from '../Modal/Modal';
+
+import './App.css'
+
+export class App extends Component {
+  state = {
+    searchQuery: '',
+    images: [],
+    page: 1,
+    per_page: 12,
+    isLoading: false,
+    loadMore: false,
+    error: null,
+    showModal: false,
+    largeImageURL: 'largeImageURL',
+    id: null,
+  };
+
+  componentDidUpdate(_, prevState) {
+  
+    const { searchQuery, page } = this.state;
+    if (prevState.searchQuery !== searchQuery || prevState.page !== page) {
+      this.getImages(searchQuery, page);
+    }
+  }
+
+  getImages = async (query, page) => {
+    this.setState({ isLoading: true });
+    if (!query) {
+      return;
+    }
+    try {
+      const { hits, totalHits } = await fetchImages(query, page);
+    
+      this.setState(prevState => ({
+        images: [...prevState.images, ...hits],
+        loadMore: this.state.page < Math.ceil(totalHits / this.state.per_page),
+      }));
+    } catch (error) {
+      this.setState({ error: error.message });
+    } finally {
+      this.setState({ isLoading: false });
+    }
+  };
+
+  formSubmit = searchQuery => {
+    this.setState({
+      searchQuery,
+      images: [],
+      page: 1,
+      loadMore: false,
+    });
+  };
+
+  onloadMore = () => {
+    this.setState(prevState => ({ page: prevState.page + 1 }));
+    this.scrollOnMoreButton();
+  };
+
+  scrollOnMoreButton = () => {
+    animateScroll.scrollToBottom({
+      duration: 1000,
+      delay: 10,
+      smooth: 'linear',
+    });
+  };
+
+  openModal = largeImageURL => {
+     this.setState({
+      showModal: true,
+      largeImageURL: largeImageURL,
+    });
+  };
+
+  closeModal = () => {
+    this.setState({
+      showModal: false,
+    });
+  };
+
+  render() {
+    const { images, isLoading, loadMore, page, showModal, largeImageURL } =
+      this.state;
+    return (
+      <>
+        <Searchbar onSubmit={this.formSubmit} />
+
+        {isLoading ? (
+          <Loader />
+        ) : (
+          <ImageGallery images={images} openModal={this.openModal} />
+        )}
+
+        {loadMore && <Button onloadMore={this.onloadMore} page={page} />}
+
+        {showModal && (
+          <Modal largeImageURL={largeImageURL} onClose={this.closeModal} />
+        )}
+      </>
+    );
+  }
+}
+
